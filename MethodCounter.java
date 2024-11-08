4fa773c1bf28f26e47582266dc3c3a0e1befb7d7
Heinrich <henry.flegel@hsw-stud.de>
2024-11-08 11:04:52 +0100

    henrys method added

--- MethodCounter.java
+++ MethodCounter.java
@@ -1,32 +1,38 @@
 import java.sql.SQLOutput;
 
 /**
-     * Die main-Methode ruft alle statischen
-     * Methoden nacheinander auf
-     */
-    public class MethodCounter {
-        public static void main(String[] args) {
-            int methodCounter = 1;
+ * Die main-Methode ruft alle statischen
+ * Methoden nacheinander auf
+ */
+public class MethodCounter {
+    public static void main(String[] args) {
+        int methodCounter = 1;
 
-            // Erste Methode
-            System.out.println("Method" + methodCounter + ":");
-            aFirstMethod();
-            methodCounter++;
-            aSecondMethod();
-            methodCounter++;
-            robertsMethod();
-            methodCounter;
-        }
-
-        private static void aFirstMethod() {
-            System.out.println("MethodCollector.aFristMethod: This method has been created by iLuckyW");
-        }
-
-        private static void aSecondMethod() {
-            System.out.println("MethodCollector.aFristMethod: This method has been created by PhoenixDE");
-        }
-
-        private static void robertsMethod() {
-            System.out.println("MethodCollector.aFristMethod: This method has been created by Robert🦖");
-        }
+        // Erste Methode
+        System.out.println("Method" + methodCounter + ":");
+        aFirstMethod();
+        methodCounter++;
+        aSecondMethod();
+        methodCounter++;
+        robertsMethod();
+        methodCounter++;
+        henrysMethod();
+        methodCounter++;
     }
+
+    private static void aFirstMethod() {
+        System.out.println("MethodCollector.aFristMethod: This method has been created by iLuckyW");
+    }
+
+    private static void aSecondMethod() {
+        System.out.println("MethodCollector.aFristMethod: This method has been created by PhoenixDE");
+    }
+
+    private static void robertsMethod() {
+        System.out.println("MethodCollector.aFristMethod: This method has been created by Robert🦖");
+    }
+
+    private static void henrysMethod() {
+        System.out.println("MethodCollector.aFristMethod: This method has been created by Henry");
+    }
+}
