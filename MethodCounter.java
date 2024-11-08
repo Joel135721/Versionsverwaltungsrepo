6eab14d291dbc90a59a8b9fc67105b2106e8e619
iLuckyW <98313436+iLuckyW@users.noreply.github.com>
2024-11-08 11:08:08 +0100

    Update MethodCounter.java

--- MethodCounter.java
+++ MethodCounter.java
@@ -18,10 +18,12 @@ public class MethodCounter {
         methodCounter++;
         henrysMethod();
         methodCounter++;
+        lenasMethod()
+        methodCounter++;
     }
 
     private static void aFirstMethod() {
-        System.out.println("MethodCollector.aFristMethod: This method has been created by iLuckyW");
+        System.out.println("MethodCollector.aFristMethod: This method has been created by Lucas");
     }
 
     private static void aSecondMethod() {
@@ -34,5 +36,9 @@ public class MethodCounter {
 
     private static void henrysMethod() {
         System.out.println("MethodCollector.aFristMethod: This method has been created by Henry");
+    }
+
+    private static void lenasMethod() {
+        System.out.println("MethodCollector.aFristMethod: This method has been created by Lena");
     }
 }
