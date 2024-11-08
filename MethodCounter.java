1b88100abe2a64c50273068854846a168659a108
lenilak <leni.lakebrink@hsw-stud.de>
2024-11-08 11:13:25 +0100

    Lenis Methode

--- MethodCounter.java
+++ MethodCounter.java
@@ -15,7 +15,9 @@ import java.sql.SQLOutput;
             aSecondMethod();
             methodCounter++;
             robertsMethod();
-            methodCounter;
+            methodCounter++;
+            lenisMethod();
+            methodCounter++;
         }
 
         private static void aFirstMethod() {
@@ -28,5 +30,9 @@ import java.sql.SQLOutput;
 
         private static void robertsMethod() {
             System.out.println("MethodCollector.aFristMethod: This method has been created by Robert🦖");
+        }
+
+        private static void lenisMethod() {
+            System.out.println("MethodCollector.aFristMethod: This method has been created by Leni:)");
         }
     }
